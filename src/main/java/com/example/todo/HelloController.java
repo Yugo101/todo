9e066573f826a6792990d5579b8673cd2a7ab388
BZS_Higashiyama <bzg.yugo.higasiyama@gmail.com>
2026-05-24 12:30:14 +0900

    Thymeleafでhello.htmlを表示する機能を追加

--- src/main/java/com/example/todo/HelloController.java
+++ src/main/java/com/example/todo/HelloController.java
@@ -1,12 +1,15 @@
 package com.example.todo;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
     @GetMapping("/hello")
-    public String hello() {
-       return "Hello World";
+    public String hello(Model model) {
+        model.addAttribute("message", "Hello World");
+       return "hello";
     }
 }
