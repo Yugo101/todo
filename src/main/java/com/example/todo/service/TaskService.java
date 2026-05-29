d336edbd9386e4edf790184242d570e97f6f43a6
BZS_Higashiyama <bzg.yugo.higasiyama@gmail.com>
2026-05-29 16:44:49 +0900

    Add task list display feature

---
+++ src/main/java/com/example/todo/service/TaskService.java
@@ -0,0 +1,22 @@
+package com.example.todo.service;
+
+import java.util.List;
+
+import com.example.todo.entity.Task;
+import com.example.todo.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TaskService {
+    private final TaskRepository taskRepository;
+
+    @Autowired
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public List<Task> taskFindAll() {
+        return taskRepository.findAll();
+    }
+}
