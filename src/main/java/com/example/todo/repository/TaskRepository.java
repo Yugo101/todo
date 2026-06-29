53451f9cd75b566157ee80a4f5874e592607a8c7
BZS_Higashiyama <bzg.yugo.higasiyama@gmail.com>
2026-06-29 16:55:44 +0900

    Add task category filter feature

--- src/main/java/com/example/todo/repository/TaskRepository.java
+++ src/main/java/com/example/todo/repository/TaskRepository.java
@@ -1,9 +1,12 @@
 package com.example.todo.repository;
 
+import com.example.todo.entity.Category;
 import com.example.todo.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface TaskRepository  extends JpaRepository<Task, Long> {
+    List<Task> findByCategory(Category category);
 }
